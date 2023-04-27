9ca16e1d587ec48a5944671796e8c8ea3bbae121
ChampionOfTheRealm <bostonmarathon69420@yahoo.com>
2023-04-27 18:17:53 -0400

    Completed the Etch-A-Sketch. Added button to change colors at random, changed the event from click to mouseover, and made it so that on every mouseover the random colors progressively get darker. Also added an animation for the Random Color Mode button so it cycles through random background colors when selected

--- main.js
+++ main.js
@@ -2,22 +2,19 @@
 const body = document.querySelector('body');
 const div = document.createElement('div');
 const button = document.createElement('button');
+const titleGridContainer = document.querySelector('.title-grid-container');
 
-const buttonContainer = body.appendChild(div);
-buttonContainer.classList.add('button-container');
-const gridContainer = body.insertAdjacentElement('afterend', div);
+const gridContainer = titleGridContainer.appendChild(div);
 gridContainer.classList.add('grid-container');
 
-//buttonContainer.classList.add('button-container')
-const gridButton = buttonContainer.appendChild(button);
-gridButton.classList.add('grid-button');
-gridButton.innerText = "Grid Size";
+const gridButton = document.querySelector('.grid-button');
+gridButton.addEventListener('click', userInputGridSize);
 
+const randomColorButton = document.querySelector('#random-color-button');
+randomColorButton.addEventListener('click', randomColorToggle)
 
 //get user input a create a X * X sized table
-function createTable(gridSize = prompt("How large would you like the grid to be?")) {
-
-    
+function createTable(gridSize) {
     
     for (j = 0; j < gridSize; j++) {
         const outterDivs = document.createElement('div');
@@ -31,19 +28,76 @@ function createTable(gridSize = prompt("How large would you like the grid to be?
         }
     }
     const innerDivs = document.querySelectorAll(".innerDiv");
-    innerDivs.forEach(divInnerContainer => divInnerContainer.addEventListener('click', changeColor));
-    
+    innerDivs.forEach(divInnerContainer => divInnerContainer.addEventListener('mouseover', changeColor));
 }
+
+function randomColor (e) {
+
+    let randomRed = Math.floor(Math.random() * 255);
+    let randomBlue = Math.floor(Math.random() * 255);
+    let randomGreen = Math.floor(Math.random() * 255);
+    let divSelected = e.target;
+    let divColor = divSelected.style.backgroundColor;
+    if (divColor != "black" && divColor != "white") {
+        console.log(divColor);
+        let newColor = divColor.slice(4, (divColor.length - 1));
+        newColor = newColor.split(", ");
+        let finalColor = [];
+        newColor.forEach(color => {
+            color = parseInt(color) - 20;
+            finalColor.push(color); 
+        });
+        let result = finalColor.toString();
+        result = finalColor.join(", ");
+        result = "rgb(" + finalColor + ")";
+        divSelected.style.backgroundColor = result;
+    } else {
+        divSelected.style.backgroundColor = `rgb(${randomRed},${randomGreen},${randomBlue})`;
+    }
+}
+    
 
 //Change the color of divs in the grid based on user input
 function changeColor(e) {
     let divSelected = e.target;
     let divSelectedColor = e.target.style.backgroundColor;
-    if (divSelectedColor == 'black') {
+    if (divSelectedColor != 'white') {
         divSelected.style.backgroundColor = 'white';
-    } else if (divSelectedColor == 'white') {
+    } else if (divSelectedColor != 'black') {
         divSelected.style.backgroundColor = 'black';
     } 
 }
 
-createTable();
+function userInputGridSize (e) {
+    if (e) {
+        gridContainer.innerHTML = "";
+        let gridSize = prompt("How large would you like the grid to be?");
+        if (gridSize <= 0 || gridSize > 64) {
+            alert("Please input a grid size range between 1 and 64");
+            gridSize = prompt("How large would you like the grid to be?");
+        }
+        createTable(gridSize);
+    } else {
+        createTable(16);
+    }
+}
+
+function randomColorToggle (e) {
+    let randomColorButtonToggle = e.target;
+    const innerDivs = document.querySelectorAll(".innerDiv");
+    if (randomColorButtonToggle.value == "off") {
+        innerDivs.forEach(divInnerContainer => divInnerContainer.removeEventListener('mouseover', changeColor));
+        innerDivs.forEach(divInnerContainer => divInnerContainer.addEventListener('mouseover', randomColor));
+        randomColorButtonToggle.removeAttribute('value');
+        randomColorButtonToggle.value = "on";
+        randomColorButtonToggle.innerText = "Select Black & White Mode";
+    } else if (randomColorButtonToggle.value == "on"){
+        innerDivs.forEach(divInnerContainer => divInnerContainer.removeEventListener('mouseover', randomColor));
+        innerDivs.forEach(divInnerContainer => divInnerContainer.addEventListener('mouseover', changeColor));
+        randomColorButtonToggle.removeAttribute('value');
+        randomColorButtonToggle.value = "off";
+        randomColorButtonToggle.innerText = "Select Random Color Mode";
+    }
+}
+
+userInputGridSize();
